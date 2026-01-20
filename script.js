d9cbf318d6e34c4ed569a153cb18102deca96fde
first-repo <thapamayank88@gmail.com>
2026-01-20 13:57:49 +0530

    AdvanceJS1

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let cursor = document.querySelector("#cursor")
-let main = document.querySelector("main")
+//globla- window
+//function ke andar- window
+//object ke andar function(method)- object
+//es6 function inside object - window
+//es5 function inside es5 function inside object ->window
+//es6 function inside es5 function inside object ->object
+//es6 function inside es6 function inside object -> window
 
-main.addEventListener("mousemove",function(dets){
-  cursor.style.left = dets.x + "px"
-  cursor.style.top = dets.y +'px'
-  cursor.style.transform = 'translate(-50%,-50%)'
-})
 
-cursor.addEventListener("click",function(){
+//call, apply, bind
+//if function ke andar this ki value ko dusre object ki value dena hai toh use krte hai
 
-})
+let obj={
+    name: "Mayank"
+}
+
+function hola(a,b,c){
+    console.log(this,a,b,c)
+}
+
+const fn = hola.bind(obj,1,2,3)
+
+fn()
