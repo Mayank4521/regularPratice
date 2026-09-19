6caef567c6afc0328d9f1433397b9332008d54ad
first-repo <thapamayank88@gmail.com>
2026-09-19 16:13:59 +0530

    moodify changes for deployment

--- backend/day-30/Backend/src/app.js
+++ backend/day-30/Backend/src/app.js
@@ -4,17 +4,18 @@ const songRouter = require("./routes/song.routes")
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path")
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+  cors({}),
 );
 app.use("/api/auth", authRouter);
 app.use("/api/songs",songRouter)
 app.use(express.static("./public"))
+app.use("*name",(req,res)=>{
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"))
+})
 
 module.exports = app;
